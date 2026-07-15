95cac3a85638547585f72feb4a29dc9369d035b5
guamanialex2007-web <guamanialex2007@gmail.com>
2026-07-15 11:27:26 -0500

    Update programa.c

--- lbaseC/programa.c
+++ lbaseC/programa.c
@@ -1,63 +1,42 @@
 #include <stdio.h>
-void completarMatrices(int Arreglo[2][2][3]) {
-    for (int plano = 0; plano < 2; plano++) {
-        char nombreMatriz = (plano == 0) ? 'A' : 'B';
-        printf("Ingreso de datos para la Matriz %c:\n", nombreMatriz);
-        
-        for (int fila = 0; fila < 2; fila++) {
-            for (int columna = 0; columna < 3; columna++) {
-                printf("Matriz %c[%d][%d]: ", nombreMatriz, fila, columna);
-                scanf("%d", &Arreglo[plano][fila][columna]);
-            }
-        }
-    }
-}
-
-void operarMatrices(int A[2][3], int B[2][3], int R[2][3], char operacion) {
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 3; j++) {
-            switch (operacion) {
-                case '+':
-                    R[i][j] = A[i][j] + B[i][j];
-                    break;
-                case '-':
-                    R[i][j] = A[i][j] - B[i][j];
-                    break;
-                case '*':
-                    R[i][j] = A[i][j] * B[i][j];
-                    break;
-            }
-        }
-    }
-}
-
-void mostrarResultado(const char* titulo, int M[2][3]) {
-    printf("\n%s:\n", titulo);
-    for (int i = 0; i < 2; i++) {
-        printf("[ ");
-        for (int j = 0; j < 3; j++) {
-            printf("%d\t", M[i][j]);
-        }
-        printf("]\n");
-    }
-}
 
 int main() {
-    int Datos[2][2][3];
+    int A[2][3], B[2][3];
     int Suma[2][3], Resta[2][3], Multiplicacion[2][3];
 
-    completarMatrices(Datos);
+    printf("Ingreso de datos para la Matriz A:\n");
+    for (int fila = 0; fila < 2; fila++) {
+        for (int columna = 0; columna < 3; columna++) {
+            printf("A[%d][%d]: ", fila, columna);
+            scanf("%d", &A[fila][columna]);
+        }
+    }
 
-    operarMatrices(Datos[0], Datos[1], Suma, '+');
-    operarMatrices(Datos[0], Datos[1], Resta, '-');
-    operarMatrices(Datos[0], Datos[1], Multiplicacion, '*');
+    printf("\nIngreso de datos para la Matriz B:\n");
+    for (int fila = 0; fila < 2; fila++) {
+        for (int columna = 0; columna < 3; columna++) {
+            printf("B[%d][%d]: ", fila, columna);
+            scanf("%d", &B[fila][columna]);
+        }
+    }
 
-    mostrarResultado("Matriz Suma (A + B)", Suma);
-    mostrarResultado("Matriz Resta (A - B)", Resta);
-    mostrarResultado("Matriz Multiplicación (A * B)", Multiplicacion);
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            Suma[i][j] = A[i][j] + B[i][j];
+            Resta[i][j] = A[i][j] - B[i][j];
+            Multiplicacion[i][j] = A[i][j] * B[i][j];
+        }
+    }
+
+    printf("\nSuma:\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d\t", Suma[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
-
 ---
 [⬅️ Volver al Índice Principal](./README.md)
